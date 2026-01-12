dcc30317e3b39e881e6a81855690d3d5223d10a4
Edris-Y <Edrisyoussef32@gmail.com>
2026-01-12 10:09:17 +0100

    Re Organisation du dossier afin de préparer la prochaine étape du projet

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Tableau de Bord</h1>
+      <p>Bienvenue sur votre espace personnel.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Page de Connexion</h1>
+      <p>Formulaire email/mot de passe.</p>
+    </div>
+  );
+}
+
+export default Login;
