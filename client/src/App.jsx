131099355b1771cc14ad7927e5567cd28ed64ce1
Lina El-HATHOUT <elhathoutlina@gmail.com>
2026-03-29 15:23:08 +0200

    Mise a jour App.jsx sur Lina/fusion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // Pages globales
 import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 // Enseignant
 import EnseignantCalendar from './enseignant/pages/EnseignantCalendar';
+import EnseignantNotifications from './enseignant/pages/EnseignantNotifications';
+import EnseignantDemandes from './enseignant/pages/EnseignantDemandes';
+import EnseignantNouvelleDemandeReservation from './enseignant/pages/EnseignantNouvelleDemandeReservation';
+import EnseignantSeanceDetails from './enseignant/pages/EnseignantSeanceDetails';
+import EnseignantCohortes from './enseignant/pages/EnseignantCohortes';
 
 // Étudiant
 import EtudiantPage from './etudiant/pages/EtudiantPage';
+import EtudiantNotifications from './etudiant/pages/EtudiantNotifications';
+import EtudiantSeanceDetails from './etudiant/pages/EtudiantSeanceDetails';
 
 // Admin
 import AdminLayout from './admin/layout/AdminLayout';
@@ -25,11 +31,26 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/calendar" element={<EnseignantCalendar />} />
+
+        <Route path="/users" element={<Navigate to="/admin/utilisateurs" replace />} />
+        <Route path="/profil" element={<Profile />} />
+
+        <Route path="/enseignant" element={<EnseignantCalendar />} />
+        <Route path="/calendar" element={<Navigate to="/enseignant" replace />} />
+        <Route path="/enseignant/notifications" element={<EnseignantNotifications />} />
+        <Route path="/enseignant/demandes" element={<EnseignantDemandes />} />
+        <Route
+          path="/enseignant/demandes/nouvelle-reservation"
+          element={<EnseignantNouvelleDemandeReservation />}
+        />
+        <Route path="/enseignant/seance/:id" element={<EnseignantSeanceDetails />} />
+        <Route path="/enseignant/cohortes" element={<EnseignantCohortes />} />
+
         <Route path="/etudiant" element={<EtudiantPage />} />
+        <Route path="/etudiant/notifications" element={<EtudiantNotifications />} />
+        <Route path="/etudiant/seance/:id" element={<EtudiantSeanceDetails />} />
 
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminDashboard />} />
@@ -39,6 +60,8 @@ function App() {
           <Route path="salles" element={<AdminSalles />} />
           <Route path="utilisateurs" element={<AdminUtilisateurs />} />
         </Route>
+
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
